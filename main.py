958d930be799218c595293f6e4ac72aa8698a2b6
MatthewPhysMath <matvey.chmyr95@mail.ru>
2025-05-03 22:55:54 +0500

    Update main.py

--- main.py
+++ main.py
@@ -70,9 +70,12 @@ async def main():
 if __name__ == "__main__":
     import asyncio
 
-    loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     try:
         loop.run_until_complete(main())
     except KeyboardInterrupt:
         pass
+    finally:
+        loop.close()
 
